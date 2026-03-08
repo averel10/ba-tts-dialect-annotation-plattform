25843b04f50bf0a2cc63646dc2873c7eb6352ec0
averel10 <achille.huenenberger@gmail.com>
2026-03-08 09:56:54 +0100

    fixed auth bug

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,18 @@ import { betterAuth } from "better-auth";
 import { localization } from "better-auth-localization";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import db from "./db";
+import { user, session, account, verification } from "./model";
 
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
       provider: "sqlite",
+      schema: {
+        user,
+        session,
+        account,
+        verification,
+      },
     }),
     emailAndPassword: {
         enabled: true,
